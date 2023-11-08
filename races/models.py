3d1adb97e052cb32c0077839aab7d12a3a5d79ec
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2023-11-08 13:01:55 -0800

    updated my models

--- races/models.py
+++ races/models.py
@@ -1,18 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+DISTANCE_CHOICES = [
+        ("5k", "5k"),
+        ("10k", "10k"),
+        ("half_marathon", "Half Marathon"),
+        ("marathon", "Marathon"),
+        ("30k", "30k"),
+        ("35k", "35k"),
+        ("50k", "50k"),
+        ("50_mile", "50 mile"),
+        ("100k", "100k"),
+        ("100_mile", "100 mile"),
+]
+
+TERRAIN_CHOICES = [
+        ("trail", "Trail"),
+        ("road", "Road"),
+]
+
 class Race(models.Model):
     name = models.CharField(max_length=150)
-    distance = models.CharField(max_length=150)
+    distance = models.CharField(
+        max_length=13,
+        choices=DISTANCE_CHOICES,
+        )
     location = models.CharField(max_length=150)
-    terrain = models.CharField(max_length=150)
-    elevation_gain = models.IntegerField()
-    date = models.DateTimeField()
+    terrain = models.CharField(
+        max_length=5,
+        choices=TERRAIN_CHOICES,
+        )
+    elevation_gain = models.PositiveIntegerField()
+    date = models.DateField()
     owner = models.ForeignKey(
         User,
         related_name="projects",
         on_delete=models.CASCADE,
-        null=True,
     )
 
     def __str__(self):
--- results/models.py
+++ results/models.py
@@ -2,21 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from races.models import Race
 
+DIVISION_CHOICES = [
+        ("m", "M"),
+        ("f", "F"),
+]
+
 class Result(models.Model):
-    time = models.IntegerField()
-    overall_place = models.IntegerField()
-    division = models.CharField(max_length=1)
-    division_place = models.IntegerField()
-    finishers = models.IntegerField()
-    race = models.ForeignKey(
+    time = models.DurationField()
+    overall_place = models.SmallIntegerField()
+    division = models.CharField(
+        max_length=1,
+        choices=DIVISION_CHOICES,
+        )
+    division_place = models.SmallIntegerField()
+    finishers = models.SmallIntegerField()
+    race = models.OneToOneField(
         Race,
         related_name="results",
         on_delete=models.CASCADE,
-        null=True
     )
     owner = models.ForeignKey(
         User,
         related_name="results",
         on_delete=models.CASCADE,
-        null=True,
     )
