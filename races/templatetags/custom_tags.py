15501bc3e29ef686fc295a1358b0915902769a32
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2023-11-09 17:51:58 -0800

    updates

--- races/templatetags/custom_tags.py
+++ races/templatetags/custom_tags.py
@@ -13,3 +13,12 @@ def days_until(value):
     today = date.today()
     delta = value - today
     return (delta.days)
+
+@register.filter
+def calculate_percentile(value, total):
+    try:
+        if total > 0:
+            return round((value / total) * 100, 2)
+    except (ValueError, ZeroDivisionError):
+        pass
+    return 0 
